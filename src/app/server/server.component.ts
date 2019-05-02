68cc0277d9882227dea58a145f12797b7b01ad98
George <georgerincon2@gmail.com>
2019-05-01 23:42:31 -0400

    initial commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+//decorators enchanced classes/components
+
+//decorators are attached by prefix @ with decoration name
+
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent { }
